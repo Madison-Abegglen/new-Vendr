086f1ce016d0e1a5fd3d9b8ff3e58808315f9802
Madison Abegglen <madison.abegglen@gmail.com>
2018-08-27 14:40:44 -0600

    just setup

---
+++ src/models/Target.js
@@ -0,0 +1,11 @@
+export default class Target {
+    constructor() {
+        this.health = data.health
+        this.name = data.name
+        this.attacks = data.attacks
+        this.items = data.items
+    }
+
+    attack(type) {
+    }
+}
--- src/store.js
+++ src/store.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Target from './models/Target'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    target: new Target({
+      health: 100,
+      name: "ScarCrow",
+      attacks: {
+        kick: 10,
+        slap: 1,
+        punch: 5
+      }
+    })
   },
   mutations: {
 
