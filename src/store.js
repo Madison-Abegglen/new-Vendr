08ac1ba29dea30d0961f0f379851eca1613fb263
Madison Abegglen <madison.abegglen@gmail.com>
2018-08-28 15:44:42 -0600

    much better now

--- src/store.js
+++ src/store.js
@@ -10,95 +10,92 @@ export default new Vuex.Store({
       machineTotal: 0.00,
       items: [{
         id: 1,
-        itemName: "Iced Vanilla Americano with Cream",
+        name: "Iced Black Vanilla Americano",
         price: 2.25,
         quantity: 5,
-        imgUrl: "",
+        imgUrl: "https://files.dutchbros.com/website/menu/drink-images/Americano_Americano_Iced.png",
         disabled: false
       },
       {
         id: 2,
-        itemName: "Iced Caramelizer with Whipped Cream",
+        name: "Blended Caramelizer with Whipped Cream",
         price: 4.00,
         quantity: 4,
-        imgUrl: "",
-        disable: false
+        imgUrl: "https://files.dutchbros.com/website/menu/drink-images/Freeze_Caramelizer_Blended.png",
+        disabled: false
       },
       {
         id: 3,
-        itemName: "Iced Kicker Cold Brew",
+        name: "Iced Kicker Cold Brew",
         price: 5.75,
         quantity: 2,
-        imgUrl: "",
+        imgUrl: "https://files.dutchbros.com/website/menu/drink-images/Cold_Brew_Caramelizer_Iced.png",
         disabled: false
       }],
       wallet: {
-        totalMoney: 20.00,
-        currency: [{
-          name: "Quarter",
-          value: .25
-        },
-        {
-          name: "Dollar",
-          value: 1.00
-        },
-        {
-          name: "5-Dollar",
-          value: 5.00
-        }]
+        currency: {
+          quarter: .25,
+          dollar: 1.00,
+          fiveDollar: 5.00
+        }
       }
     }
   },
   mutations: {
-    // create function to set the total $ to new total 
-    setTotal(state, newTotal) {
-      state.machine.total = newTotal
-    },
-    // create function to find selected item, compare quantity and price 
-    makePurchase(state, obj) {
-      let item = state.machine.find(i => i.id == obj.id) // find 
-      item.quantity = obj.quantity // quantity 
-      state.machine.machineTotal = obj.total // price 
-    },
-    // create function to find selected item, add disabled feature
-    disableClass(state, obj) {
-      let item = state.machine.find(i => i.id == obj.id)
-      item.disabled = true
-      console.log(item)
-    },
-    // create function to decrease wallet total to new total 
-    decWallet(state, newTotal) {
-
-    },
-    // reset whole machine to 0 dollars and set wallet back to original total 
-    resetVendr(state, sum) {
-
+    setCurrency(state, type) {
+      state.machine.machineTotal += state.machine.wallet.currency[type]
     }
+    // // create function to set the total $ to new total 
+    // setTotal(state, newTotal) {
+    //   state.machine.total = newTotal
+    // },
+    // // create function to find selected item, compare quantity and price 
+    // makePurchase(state, obj) {
+    //   let item = state.machine.find(i => i.id == obj.id) // find 
+    //   item.quantity = obj.quantity // quantity 
+    //   state.machine.machineTotal = obj.total // price 
+    // },
+    // // create function to find selected item, add disabled feature
+    // disableClass(state, obj) {
+    //   let item = state.machine.find(i => i.id == obj.id)
+    //   item.disabled = true
+    // },
+    // // create function to decrease wallet total to new total 
+    // decWallet(state, newTotal) {
+    //   state.machine.wallet.totalMoney
+    // },
+    // // reset whole machine to 0 dollars and set wallet back to original total 
+    // resetVendr(state, sum) {
+
+    // }
   },
   actions: {
-    // action will set current item to payload, compare item price to 
-    // money in machine, compare quantity, set new total, commit the change,
-    // and toggle disable true or false based on quantity of item left in machine.
-    purchase({ dispatch, commit, state }, payload) {
-
-    },
-    // change total in machine to original amnt + change added 
-    addMoney({ dispatch, commit, state }, payload) {
-
-    },
-    // let the new total be the sum in wallet - payload 
-    decWallet({ dispatch, commit, state }, payload) {
-
-    },
-    // action will pick currency & its btn and compare if the btn value 
-    // is more than what you currently have in wallet 
-    checkMoney({ dispatch, commit, state }, payload) {
-
-    },
-    // action will give change by setting wallet total to its current total 
-    // + current machine total 
-    giveChange({ dispatch, commit, state }, payload) {
-
+    addCurrency({ dispatch, commit }, type) {
+      commit("setCurrency", type)
     }
+    //   // action will set current item to payload, compare item price to 
+    //   // money in machine, compare quantity, set new total, commit the change,
+    //   // and toggle disable true or false based on quantity of item left in machine.
+    //   purchase({ dispatch, commit, state }, payload) {
+
+    //   },
+    //   // change total in machine to original amnt + change added 
+    //   addMoney({ dispatch, commit, state }, payload) {
+
+    //   },
+    //   // let the new total be the sum in wallet - payload 
+    //   decWallet({ dispatch, commit, state }, payload) {
+
+    //   },
+    //   // action will pick currency & its btn and compare if the btn value 
+    //   // is more than what you currently have in wallet 
+    //   checkMoney({ dispatch, commit, state }, payload) {
+
+    //   },
+    //   // action will give change by setting wallet total to its current total 
+    //   // + current machine total 
+    //   giveChange({ dispatch, commit, state }, payload) {
+
+    // }
   }
 })
--- src/views/Vendr.vue
+++ src/views/Vendr.vue
@@ -3,17 +3,30 @@ money in machine, and add currency btn -->
 <template>
   <div class="vendr">
     <h1>{{machine.name}}</h1>
+    <h4>Machine Total: <br>${{machine.machineTotal}}</h4>
+    <div class="row">
+      <div class="col-4" v-for="item in machine.items" :key="item.id">
+        <h2>{{item.name}}</h2>
+        <img :src="item.imgUrl" alt=""/>
+        <h4>${{item.price}}</h4>
+      </div>
+    </div>
+    <button class="btn" type="button" @click="addCurrency('quarter')">Quarter</button>
+    <button class="btn" type="button" @click="addCurrency('dollar')">Dollar</button>
+    <button class="btn" type="button" @click="addCurrency('fiveDollar')">5-Dollar</button>
   </div>
 </template>
-
 
 <script>
 export default {
   methods: {
+    addCurrency(type) {
+      this.$store.dispatch("addCurrency", type);
+    }
     // correspond with what you wrote in the store
-    purchase(value) {},
-    moveTheMoney() {},
-    getMoneyBack() {}
+    // purchase(value) {},
+    // moveTheMoney() {},
+    // getMoneyBack() {}
   },
   computed: {
     // correspond with what you wrote in store, remember to return**
